a59325a3bddee72936d8bdff713f971581ccfdaf
BOVE <graziano.bove@capgemini.com>
2026-09-26 11:28:36 +0200

    feat: complete automatic weather notification scheduler

--- src/services/apiForecastService.ts
+++ src/services/apiForecastService.ts
@@ -1,6 +1,4 @@
-//const API_BASE_URL = "https://site--meteocompare-api--ddx7k442y97b.code.run";
-
-const API_BASE_URL = "http://10.0.2.2:3000";
+const API_BASE_URL = "https://site--meteocompare-api--ddx7k442y97b.code.run";
 
 export async function getForecastsByCity(city: string, day: number = 0) {
   const url = `${API_BASE_URL}/forecast/${city.toLowerCase()}?day=${day}`;
